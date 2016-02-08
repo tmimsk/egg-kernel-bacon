126dafabeb343e514bfae5757c5ce58ca9a57f87
YoshiShaPow <yoshipga@gmail.com>
2016-02-07 19:26:33 -0800

    cpuinit: get rid of __cpuinit
    * based on showp1984's commit
    
    This commit is the result of:
    
    find -name "*.c" | xargs sed -i "s/ __cpuinit / /g"
    find -name "*.c" | xargs sed -i "s/ __cpuexit / /g"
    find -name "*.c" | xargs sed -i "s/ __cpuinitdata//g"
    find -name "*.c" | xargs sed -i "s/ __cpuinit$//g"
    find arch/ -name "*.h" | xargs sed -i "s/ __cpuinit//g"
    find -name "*.c" | xargs sed -i "s/^__cpuinit //g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: YoshiShaPow <yoshipga@gmail.com>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -703,7 +703,7 @@ void send_remote_softirq(struct call_single_data *cp, int cpu, int softirq)
 }
 EXPORT_SYMBOL(send_remote_softirq);
 
-static int __cpuinit remote_softirq_cpu_notify(struct notifier_block *self,
+static int remote_softirq_cpu_notify(struct notifier_block *self,
 					       unsigned long action, void *hcpu)
 {
 	/*
@@ -732,7 +732,7 @@ static int __cpuinit remote_softirq_cpu_notify(struct notifier_block *self,
 	return NOTIFY_OK;
 }
 
-static struct notifier_block __cpuinitdata remote_softirq_cpu_notifier = {
+static struct notifier_block remote_softirq_cpu_notifier = {
 	.notifier_call	= remote_softirq_cpu_notify,
 };
 
@@ -842,7 +842,7 @@ static void takeover_tasklets(unsigned int cpu)
 }
 #endif /* CONFIG_HOTPLUG_CPU */
 
-static int __cpuinit cpu_callback(struct notifier_block *nfb,
+static int cpu_callback(struct notifier_block *nfb,
 				  unsigned long action,
 				  void *hcpu)
 {
@@ -857,7 +857,7 @@ static int __cpuinit cpu_callback(struct notifier_block *nfb,
 	return NOTIFY_OK;
 }
 
-static struct notifier_block __cpuinitdata cpu_nfb = {
+static struct notifier_block cpu_nfb = {
 	.notifier_call = cpu_callback
 };
 
