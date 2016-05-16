cooldroid/android_kernel_oneplus_msm8974

cpuquiet: introduce

arch/arm/mach-msm/msm_rq_stats.c
@@ -32,6 +32,9 @@
 #include <asm/smp_plat.h>
 #include <linux/suspend.h>
 
+/* only enable on demand if needed */
+static bool load_stats_enabled = true;
+
 #define MAX_LONG_SIZE 24
 #define DEFAULT_RQ_POLL_JIFFIES 1
 #define DEFAULT_DEF_TIMER_JIFFIES 5
@@ -53,6 +56,7 @@ struct cpu_load_data {
 };
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
+
 
 static inline cputime64_t get_cpu_iowait_time(unsigned int cpu,
 							cputime64_t *wall)
@@ -196,6 +200,34 @@ static int system_suspend_handler(struct notifier_block *nb,
 	return NOTIFY_OK;
 }
 
+void enable_rq_load_calc(bool on)
+{
+	int cpu;
+
+	if (on != load_stats_enabled){
+		load_stats_enabled = on;
+
+		pr_info("Enable rq_stats load calculation %d\n", load_stats_enabled);
+		if (load_stats_enabled) {
+			// clear data
+			for_each_possible_cpu(cpu) {
+				struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+
+				pcpu->prev_cpu_idle = 0;
+				pcpu->prev_cpu_wall = 0;
+				pcpu->avg_load_maxfreq = 0;
+			}
+
+			cpufreq_register_notifier(&freq_transition,
+					CPUFREQ_TRANSITION_NOTIFIER);
+			register_hotcpu_notifier(&cpu_hotplug);
+		} else {
+			cpufreq_unregister_notifier(&freq_transition,
+					CPUFREQ_TRANSITION_NOTIFIER);
+			unregister_hotcpu_notifier(&cpu_hotplug);
+		}
+	}
+}
 
 static ssize_t hotplug_disable_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
@@ -394,9 +426,11 @@ static int __init msm_rq_stats_init(void)
 	}
 	freq_transition.notifier_call = cpufreq_transition_handler;
 	cpu_hotplug.notifier_call = cpu_hotplug_handler;
-	cpufreq_register_notifier(&freq_transition,
+	if (load_stats_enabled){
+		cpufreq_register_notifier(&freq_transition,
 					CPUFREQ_TRANSITION_NOTIFIER);
-	register_hotcpu_notifier(&cpu_hotplug);
+		register_hotcpu_notifier(&cpu_hotplug);
+	}
 
 	return ret;
 }

tmimsk/egg-kernel-bacon

Conflicts:
	arch/arm/mach-msm/msm_rq_stats.c
	include/linux/sched.h
	kernel/sched/fair.c
	kernel/sched/sched.h

arch/arm/mach-msm/msm_rq_stats.c
@@ -32,6 +32,9 @@
 #include <asm/smp_plat.h>
 #include <linux/suspend.h>
 
+/* only enable on demand if needed */
+static bool load_stats_enabled = true;
+
 #define MAX_LONG_SIZE 24
 #define DEFAULT_RQ_POLL_JIFFIES 1
 #define DEFAULT_DEF_TIMER_JIFFIES 5
@@ -190,6 +193,34 @@ static int system_suspend_handler(struct notifier_block *nb,
 	return NOTIFY_OK;
 }
 
+void enable_rq_load_calc(bool on)
+{
+	int cpu;
+
+	if (on != load_stats_enabled){
+		load_stats_enabled = on;
+
+		pr_info("Enable rq_stats load calculation %d\n", oad_stats_enabled);
+		if (load_stats_enabled) {
+			// clear data
+			for_each_possible_cpu(cpu) {
+				struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+
+				pcpu->prev_cpu_idle = 0;
+				pcpu->prev_cpu_wall = 0;
+				pcpu->avg_load_maxfreq = 0;
+			}
+
+			cpufreq_register_notifier(&freq_transition,
+					CPUFREQ_TRANSITION_NOTIFIER);
+			register_hotcpu_notifier(&cpu_hotplug);
+		} else {
+			cpufreq_unregister_notifier(&freq_transition,
+					CPUFREQ_TRANSITION_NOTIFIER);
+			unregister_hotcpu_notifier(&cpu_hotplug);
+		}
+	}
+}
 
 static ssize_t hotplug_disable_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
@@ -372,9 +403,11 @@ static int __init msm_rq_stats_init(void)
 	}
 	freq_transition.notifier_call = cpufreq_transition_handler;
 	cpu_hotplug.notifier_call = cpu_hotplug_handler;
-	cpufreq_register_notifier(&freq_transition,
+	if (load_stats_enabled){
+		cpufreq_register_notifier(&freq_transition,
 					CPUFREQ_TRANSITION_NOTIFIER);
 	register_hotcpu_notifier(&cpu_hotplug);
+	}
 
 	return ret;
 }